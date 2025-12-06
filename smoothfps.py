fc9ba0f7ce742bf2048b82c45bb6dd0cd744f183
jameprime6 <jameprime6@gmail.com>
2025-12-06 07:07:10 +0630

    Update smoothfps.py

--- smoothfps.py
+++ smoothfps.py
@@ -10,28 +10,19 @@ print()
 print()
 
 print("""\033[96m
-           | ░█████╗░███╗░░██╗██████╗░██████╗░██╗░█████╗░
-           | ██╔══██╗████╗░██║██╔══██╗██╔══██╗██║██╔══██╗
-           | ███████║██╔██╗██║██║░░██║██████╔╝██║██║░░██║
-           | ██╔══██║██║╚████║██║░░██║██╔══██╗██║██║░░██║
-           | ██║░░██║██║░╚███║██████╔╝██║░░██║██║╚█████╔╝
-           | ╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═╝░╚════╝░
-           |
-           | ██████╗░░█████╗░██████╗░███████╗████████╗
-           | ██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝
-           | ██████╦╝███████║██████╔╝█████╗░░░░░██║░░░
-           | ██╔══██╗██╔══██║██╔══██╗██╔══╝░░░░░██║░░░
-           | ██████╦╝██║░░██║██║░░██║███████╗░░░██║░░░
-           | ╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░
+           |  ██████╗ ██████╗ ██████╗ ███████╗████████╗███████╗██╗   ██╗
+           |  ██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔════╝╚██╗ ██╔╝
+           |  ██████╔╝██████╦╝██████╔╝█████╗░░░░░██║░░░█████╗░░ ▚████╔╝ 
+           |  ██╔══██╗██╔══██╗██╔══██╗██╔══╝░░░░░██║░░░██╔══╝░░  ╚██╔╝  
+           |  ██████╦╝██████╦╝██║░░██║███████╗░░░██║░░░███████╗   ██║   
+           |  ╚═════╝ ╚═════╝ ╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚══════╝   ╚═╝   
 """)
 
-print("                    [*] OWNER : CODETEAM")
-print("                    [*] IG : @code07777")
-print()
+print("                     [*] CODETEAM GFX TOOL")
+print("                     [*] Created by @code07777")
 print()
 print()
 print("\033[96m      ** MENU GAMES **")
-print()
 print()
 print("  _________________________")
 print("  |   [1] MOBILE LEGENDS   |")
@@ -40,28 +31,28 @@ print("  |   [3] CALL OF DUTY     |")
 print("  _________________________")
 print()
 print()
+
 input("\033[0m[*] Select Your Favorite Game : ")
 print()
 print()
 print("\033[92m[*] Entering Menu FPS....")
 
 # Loading animation
-for i in range(10):
+for i in range(14):
     print("[*] Please wait...")
     time.sleep(0.5)
 
-print("[*] Done 👍 ")
+print("[*] Done 👍")
 time.sleep(2)
 print()
 print()
 
 print("\033[96m      ** MENU FPS **")
 print()
-print()
 print("  ___________________")
 print("  |   [1] 120 FPS    |")
-print("  |   [2] 90  FPS    |")
-print("  |   [3] 60  FPS    |")
+print("  |   [2]  90 FPS    |")
+print("  |   [3]  60 FPS    |")
 print("  ___________________")
 print()
 print()
@@ -69,14 +60,14 @@ print()
 input("\033[0m[*] Select The better FPS : ")
 time.sleep(1)
 
-print("\033[96m[*] Downloading (15mb size)...")
+print("\033[96m[*] Downloading (15mb size)... ")
 time.sleep(1)
 print("[*] Please wait a few seconds...")
 time.sleep(4)
 print()
-print("[*] Success download")
+print("[*] Success download ✔")
 time.sleep(1)
-print("[*] Your phone now in SMOOTH MODE ⚡")
+print("[*] Your phone is now in SMOOTH MODE ⚡")
 time.sleep(2)
 print()
 
@@ -87,4 +78,4 @@ time.sleep(1)
 print("1")
 time.sleep(2)
 
-print("\033[91m[*] Please reboot your device.")
+print("\033[91m[*] Please Reboot Your Device.")
